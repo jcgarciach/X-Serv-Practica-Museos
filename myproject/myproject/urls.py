4c578239f3a29b6bf3cae415a3a4c7c8643d159e
Jose Carlos  García Chillón <jcgarchi@MacBook-Pro-de-Jose.local>
2018-05-12 19:21:34 +0200

    Urls que creo necesarias no se si necesitare alguna mas

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from museos import views
+from django.contrib.auth.views import login, logout
+
+urlpatterns = [
+    url(r'^/?$' , views.pagprincipal, name = 'Muestra la página principal'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^css/style.css$', views.css, name =  'Nos sirve el CSS'),
+    url(r'^museos/?$', views.museos, name = 'Muestra la página con todos los museos'),
+    url(r'^museos/(.*)/?$', views.pagmuseo, name = 'Muestra la página de un museo'),
+    url(r'^(.*)/xml/?$', views.xml, name = 'Canal XML'),
+    url(r'^rss/?$', views.rss, name = 'Canal RSS para comentarios'),
+    url(r'^login/?$', views.loginUser, name = 'Login de los usuarios'),
+    url(r'^logout/?$', views.logoutUser, name = 'Logout de los usuarios'),
+    url(r'^about/?$', views.about, name = 'Muestra la página about'),
+    url(r'(.*)', views.pagUser, name = 'Muestra la pagina personal de un usuario'),
+]
