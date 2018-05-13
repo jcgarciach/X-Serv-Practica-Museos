586f03c316bbecbec70a66b670ba4ac04e17e2e1
Jose Carlos  García Chillón <jcgarchi@MacBook-Pro-de-Jose.local>
2018-05-13 20:04:05 +0200

    Realizadas algunas funciones

--- myproject/museos/views.py
+++ myproject/museos/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Museo, Comentario, Preferencia, Registro
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.template import loader
+import urllib
 
-# Create your views here.
+
+
+
+
+
+@csrf_exempt
+def Principal(request):
+    if request.method == 'GET':
+        
+    else:
+               
+    return HttpResponse()
+        
+def Login(request):
+    if request.method == "POST":
+        nick = request.POST['username']
+        password = request.POST['password']
+        usuario = authenticate(username = nombre, password = contrasenia)
+        if usuario is not None:
+            if usuario.is_active:
+                login(request, usuario)
+                
+    return HttpResponseRedirect('/')
+    
+    
+def Logout(request):
+    if request.method == "POST":
+        logout(request)
+        
+    return HttpResponseRedirect('/')
+    
+
+    
+        
